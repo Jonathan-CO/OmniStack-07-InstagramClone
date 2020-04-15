cc34b4a6ed12505498828c0cd3b04d8e2c3e74c2
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-15 19:14:21 -0300

    create PostController with create and index; add multer

--- backend/src/config.js
+++
@@ -1,3 +0,0 @@
-module.exports ={
-    connectionString: 'mongodb+srv://devradar:exenidospyb@cluster0-exlbd.mongodb.net/test?retryWrites=true&w=majority'
-}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,18 @@
 const express = require ('express');
-const routes = express.Router()
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+const PostController = require('./controllers/PostController');
+
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
+
 
 routes.get('/' , (req, res)=>{
     return res.json({message: "Hello InstagramClone! Bem-vindo!!"})
 })
+
+routes.get('/posts', PostController.index)
+routes.post('/posts', upload.single('image'), PostController.store)
 
 module.exports = routes;

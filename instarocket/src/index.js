60bb991c9b7ad2dc479afe7152074d1f85afb536
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-16 20:05:00 -0300

    init repo mobile

---
+++ instarocket/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  View,  Text
+} from 'react-native';
+
+function App(){
+  return (
+    <View>
+      <Text>Hello Omnistack 07</Text>
+    </ View>
+  );
+};
+
+export default App;

5e31e73e6a1ec538df1d7ea045d1f505898848b9
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-16 13:36:21 -0300

    create page new, and do like, list and create post with access to db

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+
 import './Feed.css'
 
 import more from '../assets/more.svg'
@@ -6,58 +8,55 @@ import like from '../assets/like.svg'
 import comment from '../assets/comment.svg'
 import send from '../assets/send.svg'
 
-function Feed (){
+function Feed() {
+    const [feed, setFeed] = useState([]);
+
+    useEffect(() => {
+        async function getPosts() {
+            const response = await api.get('posts')
+            setFeed(response.data)
+        }
+        getPosts();
+    }, [])
+
+    async function handleLike(id){
+        await api.post(`posts/${id}/like`)
+    }
+
     return (
         <section id="post-list">
-            <article>
-                <header>
-                    <div className="user-info">
-                        <span> Jonathan da Cunha</span>
-                        <span className="place"> Diamantina </span>
-                    </div>
+            {feed.map(post => (
+                <article key={post._id}>
+                    <header>
+                        <div className="user-info">
+                            <span>{post.author}</span>
+                            <span className="place">{post.place}</span>
+                        </div>
 
-                    <img src={more} alt="Mais"/>
-                </header>
+                        <img src={more} alt="Mais" />
+                    </header>
 
-                <img src="http://localhost:3333/files/precasamento.png" alt=""/>
-                <footer>
-                    <div className="actions">
-                        <img src={like} alt=""/>
-                        <img src={comment} alt=""/>
-                        <img src={send} alt=""/>
-                    </div>
+                    <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+                    <footer>
+                        <div className="actions">
+                            <button type="button" onClick={()=>handleLike(post._id)}>
+                                <img src={like} alt="" />
+                            </button>
 
-                    <strong>900 cutidas</strong>
-                    <p> Pre-casamento Jonathan & Delaine
-                        <span>#precasamento</span>
-                    </p>
-                </footer>
-            </article>
+                            <img src={comment} alt="" />
 
-            <article>
-                <header>
-                    <div className="user-info">
-                        <span> Jonathan da Cunha</span>
-                        <span className="place"> Diamantina </span>
-                    </div>
+                            <img src={send} alt="" />
 
-                    <img src={more} alt="Mais"/>
-                </header>
+                        </div>
 
-                <img src="http://localhost:3333/files/precasamento.png" alt=""/>
-                <footer>
-                    <div className="actions">
-                        <img src={like} alt=""/>
-                        <img src={comment} alt=""/>
-                        <img src={send} alt=""/>
-                    </div>
+                        <strong>{post.likes} cutidas</strong>
+                        <p> {post.description}
+                            <span>{post.hashtags}</span>
+                        </p>
+                    </footer>
+                </article>
+            ))}
 
-                    <strong>900 cutidas</strong>
-                    <p> Pre-casamento Jonathan & Delaine
-                        <span>#precasamento</span>
-                    </p>
-                </footer>
-            </article>
         </section>
     )
 }
--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../services/api';
+import './New.css'
 
-function New (){
+function New (props){
+    const [image, setImage] = useState(null);
+    const [author, setAuthor] = useState('');
+    const [place, setPlace] = useState('');
+    const [description, setDescription] = useState('');
+    const [hashtags, seetHashtags] = useState('');
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        
+        const data = new FormData();
+
+        data.append('image', image);
+        data.append('author', author);
+        data.append('place', place);
+        data.append('description', description);
+        data.append('hashtags', hashtags);
+
+        await api.post('/posts', data);
+        props.history.push('/')
+    }
+
+
     return (
-        <h1>New</h1>
+        <form id="new-post" onSubmit={handleSubmit}>
+            <input 
+                type="file"
+                onChange={ e => setImage(e.target.files[0])}
+            />
+            <input 
+                type="text"
+                name="author"
+                placeholder="Autor do post"
+                value={author}
+                onChange={e => setAuthor(e.target.value)}
+            />
+            <input 
+                type="text"
+                name="place"
+                placeholder="Local do post"
+                value={place}
+                onChange={e => setPlace(e.target.value)}
+            />
+            <input 
+                type="text"
+                name="description"
+                placeholder="Descrição do post"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+            />
+            <input 
+                type="text"
+                name="hashtags"
+                placeholder="Hastags do post"
+                value={hashtags}
+                onChange={e => seetHashtags(e.target.value)}
+            />
+            <button type="submit">Enviar</button>
+        </form>
     )
 }
 

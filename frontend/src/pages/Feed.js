7a04b4ebf4ac5f8fdf63504b633edd96341b0082
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2020-04-16 11:57:07 -0300

    finished design frontend of feed page

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,8 +1,64 @@
 import React from 'react';
+import './Feed.css'
+
+import more from '../assets/more.svg'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import send from '../assets/send.svg'
 
 function Feed (){
     return (
-        <h1>Feed</h1>
+        <section id="post-list">
+            <article>
+                <header>
+                    <div className="user-info">
+                        <span> Jonathan da Cunha</span>
+                        <span className="place"> Diamantina </span>
+                    </div>
+
+                    <img src={more} alt="Mais"/>
+                </header>
+
+                <img src="http://localhost:3333/files/precasamento.png" alt=""/>
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt=""/>
+                        <img src={comment} alt=""/>
+                        <img src={send} alt=""/>
+                    </div>
+
+                    <strong>900 cutidas</strong>
+                    <p> Pre-casamento Jonathan & Delaine
+                        <span>#precasamento</span>
+                    </p>
+                </footer>
+            </article>
+
+            <article>
+                <header>
+                    <div className="user-info">
+                        <span> Jonathan da Cunha</span>
+                        <span className="place"> Diamantina </span>
+                    </div>
+
+                    <img src={more} alt="Mais"/>
+                </header>
+
+                <img src="http://localhost:3333/files/precasamento.png" alt=""/>
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt=""/>
+                        <img src={comment} alt=""/>
+                        <img src={send} alt=""/>
+                    </div>
+
+                    <strong>900 cutidas</strong>
+                    <p> Pre-casamento Jonathan & Delaine
+                        <span>#precasamento</span>
+                    </p>
+                </footer>
+            </article>
+        </section>
     )
 }
 
